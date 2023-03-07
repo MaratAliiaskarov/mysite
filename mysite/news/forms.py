7d02bd0e474a2fde3498121ef64d737b8d5bfb3b
apple </Kvadrat2020/>
2023-03-07 16:42:34 -0500

    created class News, updated views and style/css created function save for data base

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -3,7 +3,11 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'rows': 5
+        }))
+    is_published = forms.BooleanField(initial=True)
+    category = forms.ModelChoiceField(empty_label='Select Category', queryset=Category.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
