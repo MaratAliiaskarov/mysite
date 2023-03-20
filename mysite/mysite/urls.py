d6ebf19679eef1dc47c5d7631925243d0c2c50b2
apple </Kvadrat2020/>
2023-03-20 11:45:42 -0400

    created django debug function

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,12 +18,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
 ]
 
-
 if settings.DEBUG:
+    from django.urls import include, path
+
+    urlpatterns = [
+                      path('__debug__/', include('debug_toolbar.urls')),
+                  ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
