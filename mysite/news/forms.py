e58d254b71a787ee12a20b55157d6208f9a2d972
apple </Kvadrat2020/>
2023-04-03 15:49:30 -0400

    updated functionality registration and messages

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,6 +2,26 @@ from django import forms
 from .models import News
 import re
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='User name', widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': "off"}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='E-mail', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
 
 class NewsForm(forms.ModelForm):
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,14 +5,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
 from .models import News, Category
-from .forms import NewsForm
+from .forms import NewsForm, UserRegisterForm
 from .utils import MyMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'You successfully registered')
@@ -20,7 +20,7 @@ def register(request):
         else:
             messages.error(request, 'Registration ERROR')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'news/register.html', {"form": form})
 
 def login(request):
